fc1b74762a3a2ac6ed0fd85047a2c315c08b32a1
ChristalCamillo <christal.b.camillo@gmail.com>
2024-03-01 12:41:37 -0300

    explicação conexoesDocumentos

--- servidor/utils/conexoesDocumentos.js
+++ servidor/utils/conexoesDocumentos.js
@@ -1,3 +1,7 @@
+// Recebemos o nome do documento por parâmetro e, com o método filter, filtramos as conexões por aquelas que possuem a propriedade nomeDocumento igual ao nome passado por parâmetro. 
+// Em seguida, com o método map, reescrevemos o array filtrado para um array apenas com os nomes dos usuários. 
+// Por fim, retornamos o resultado de todas essas operações, concluindo a responsabilidade da função.
+
 const conexoesDocumentos = [];
 
 function adicionarConexao(conexao) {
